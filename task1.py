4eeca9e640fee9bc4c81ee410959a4d24016d54a
Karen Handy <karen.handy@cmcss.net>
2025-12-19 10:46:34 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,21 +2,21 @@ slices = party_pizza_mini + large + medium
 print(f'Total number of slices {total_slices}')
 
 people += 1
-share = slices % people
-leftover = slices // people
+leftover = slices % people
+share = slices // people
 print(f"Each person gets: {share}")
 print(f"Leftover slices: {leftover}")
 
 people += 2 #Eric and Brandon are coming too.
-share = slices % people
-leftover = slices// people
+leftover = slices % people
+share = slices// people
 print(f"Each person gets: {share}")
 print(f"Leftover slices: {leftover}")
 
 
 slices = (party_pizza_mini*2) + large + medium
-share = slices % people
-leftover = slices // people
+leftover = slices % people
+share = slices // people
 print(f"Each person gets: {share}")
 print(f"Leftover slices: {leftover}")
 print("...for Mr. Hollow Leg")
